db6f52724856d8437a86c5e72ccd0145acbeb392
M1SH4-cmd <6el0zu61979@mail.ru>
2025-02-10 22:37:39 +0700

    More notes added

--- Notes/Note2/src/main.cpp
+++ Notes/Note2/src/main.cpp
@@ -1,8 +1,63 @@
 #include <iostream>
-#include <exception>
+#include <exeption>
+
+
+class DivisionByZeroException : public std::exception
+{
+	const char* what() const noexcept override
+	{
+		return "DivisionByZeroException";
+	}
+};
+
+
+void divide(int &divident, int divisor)
+{
+	if (divisor == 0)
+	{
+		throw DivisionByZeroException();
+	}
+	divident /= divisor;
+
+}
+
+int distribute(int apples, int count)
+{
+	if (apples < 0 || count > apples) throw std::invalid_argument(apples < 0 ? "apples" : "count");
+	if (apples > 100) throw apples;
+	return divide(apples, count);
+}
 
 
 int main() {
 
-	return 0;
+	int apples;
+	int count;
+	bool input = true;
+
+
+	while (input)
+	{
+
+		std::cin >> apples;
+		std::cin >> count;
+		try
+		{
+			std::cout << distribute(apples, count) << std::endl;
+			input = false;
+		}
+		catch (const DivizionByZeroException& x)
+		{
+			input = false;
+		}
+		catch (const std::invalid_argument& x)
+		{
+			std::cerr << "Caught exception:\t" << x.what() << std::endl;
+		}
+		catch (...)
+		{
+			std::cerr << "Caught some exception..." << std::endl;
+		}
+
+	}
 }
